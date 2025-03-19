80cb40d34cc5b2c522c819e7717542474b38cb2c
Jay Tan <jay@keq.com.au>
2025-03-20 10:32:04 +1100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import MapDisplay from './components/MapDisplay/MapDisplay';
+
+export default function App() {
+  return (
+    <Container maxWidth="xl">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+          Weather Stations
+        </Typography>
+	<MapDisplay />
+      </Box>
+    </Container>
+  );
+}
---
+++ src/components/MapDisplay/MapDisplay.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+
+export default function MapDisplay() {
+  return (
+    <>
+      <Typography>Map</Typography>
+    </>
+  );
+}
