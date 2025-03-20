6cd4fe168acf418ce82ed37e811cb814f0f92567
Jay Tan <jay@keq.com.au>
2025-03-20 13:02:35 +1100

    remove test data

--- src/components/MapDisplay/MapDisplay.tsx
+++ src/components/MapDisplay/MapDisplay.tsx
@@ -10,10 +10,8 @@ import {
   useAdvancedMarkerRef
 }
 from '@vis.gl/react-google-maps';
-import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
 import { useGetWeatherStations } from "../../hooks/useGetWeatherStations";
 
 const MarkerWithInfoWindow = ({poi}) => {
@@ -80,7 +78,7 @@ const PoiMarkers = (props: { pois }) => {
 
   return (
     <>
-      {props.pois.map( (poi) => (
+      {props.pois.map((poi) => (
 	<MarkerWithInfoWindow poi={poi} key={poi.id} />
       ))}
     </>
@@ -92,27 +90,6 @@ export default function MapDisplay() {
 
   const { isLoading, error, data } = useGetWeatherStations();
 
-  console.log(data);
-
-  
-  const locations = [
-    {key: 'operaHouse', location: { lat: -33.8567844, lng: 151.213108  }},
-    {key: 'tarongaZoo', location: { lat: -33.8472767, lng: 151.2188164 }},
-    {key: 'manlyBeach', location: { lat: -33.8209738, lng: 151.2563253 }},
-    {key: 'hyderPark', location: { lat: -33.8690081, lng: 151.2052393 }},
-    {key: 'theRocks', location: { lat: -33.8587568, lng: 151.2058246 }},
-    {key: 'circularQuay', location: { lat: -33.858761, lng: 151.2055688 }},
-    {key: 'harbourBridge', location: { lat: -33.852228, lng: 151.2038374 }},
-    {key: 'kingsCross', location: { lat: -33.8737375, lng: 151.222569 }},
-    {key: 'botanicGardens', location: { lat: -33.864167, lng: 151.216387 }},
-    {key: 'museumOfSydney', location: { lat: -33.8636005, lng: 151.2092542 }},
-    {key: 'maritimeMuseum', location: { lat: -33.869395, lng: 151.198648 }},
-    {key: 'kingStreetWharf', location: { lat: -33.8665445, lng: 151.1989808 }},
-    {key: 'aquarium', location: { lat: -33.869627, lng: 151.202146 }},
-    {key: 'darlingHarbour', location: { lat: -33.87488, lng: 151.1987113 }},
-    {key: 'barangaroo', location: { lat: - 33.8605523, lng: 151.1972205 }},
-  ];
-  
   return (
     <>
       <APIProvider apiKey={import.meta.env.VITE_MAPS_API_KEY} onLoad={() => console.log('Maps API has loaded.')}>
